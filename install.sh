9f7a2ed4cbc098a5ad251117c2b9c364358398a2
ihopenot <984430652@qq.com>
2023-10-11 16:13:29 +0800

    update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
+SHELL_FOLDER=$(dirname $(readlink -f "$0"))
+cd $SHELL_FOLDER
 
-cp tmux.conf ~/.tmux.conf
-cp zshrc ~/.zshrc
+# tmux
+cp tmux/tmux.conf ~/.tmux.conf
+
+# oh-my-zsh
+cp ohmyzsh/zshrc ~/.zshrc
+touch ~/.custom.rc
+cp ohmyzsh/virtualenv.plugin.zsh ~/.oh-my-zsh/plugins/virtualenv/virtualenv.plugin.zsh
