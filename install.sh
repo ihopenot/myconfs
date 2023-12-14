1054848be5a756fb09829e188de37eb8f5bdc63f
Ihopenot <984430652@qq.com>
2023-12-14 13:38:52 +0800

    add install commands

--- install.sh
+++ install.sh
@@ -1,11 +1,17 @@
 #!/bin/sh
 SHELL_FOLDER=$(dirname $(readlink -f "$0"))
 cd $SHELL_FOLDER
+set -x
 
 # tmux
+sudo apt install tmux
 cp tmux/tmux.conf ~/.tmux.conf
 
 # oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 cp ohmyzsh/zshrc ~/.zshrc
 touch ~/.custom.rc
 cp ohmyzsh/virtualenv.plugin.zsh ~/.oh-my-zsh/plugins/virtualenv/virtualenv.plugin.zsh
+cd ~
+source ~/.zshrc
+cd -
